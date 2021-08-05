f80e8e2c307b71eb96e242184ad0caf737f3621a
Spyrex <malikfk100@gmail.com>
2021-08-05 12:17:55 +0200

    Refactoring

--- addons/SmoothScroll/SmoothScroll.gd
+++ addons/SmoothScroll/SmoothScroll.gd
@@ -1,53 +1,58 @@
 extends Control
 
 var v = Vector2(0,0) #current velocity
-var just_stop_under = 0.01
+var just_stop_below = 0.01
 export(float, -10, -1) var multi = -2 #speed of one input
 var is_grabbed = false
 
 var over_drag_multiplicator_top = 1
 var over_drag_multiplicator_bottom = 1
 
+var bottom_pos
+var top_pos
+
+export(float, 0, 1) var damping = 0.3
+
+var margin = 10
+
 func _process(delta):
 	var d = delta
-	var bottom_pos = get_children()[0].rect_position.y + get_children()[0].rect_size.y - self.rect_size.y
-	var top_pos = get_children()[0].rect_position.y
-	# If overdragged:
-	if bottom_pos < 0 :
-		over_drag_multiplicator_bottom = 1/abs(bottom_pos)*10
-	else:
-		over_drag_multiplicator_bottom = 1
-	if top_pos > 0:
-		over_drag_multiplicator_top = 1/abs(top_pos*top_pos)*10
-	else:
-		over_drag_multiplicator_top = 1
-	v *= 0.9
-	if v.length() <= just_stop_under:
+	bottom_pos = get_children()[0].rect_position.y + get_children()[0].rect_size.y - self.rect_size.y + margin
+	top_pos = get_children()[0].rect_position.y - margin
+
+
+	if v.length() <= just_stop_below:
 		v = Vector2(0,0)
 	
-	if not is_grabbed:
-		if bottom_pos < -20 :
-			v.y = +2
-		if top_pos > 20:
-			v.y = -2
-			
-	if is_grabbed:
-		if bottom_pos < -20 || top_pos > 20:
-			v.y = v.y / 2
-
+	if !(bottom_pos < 0 && v.y <= 0) && !(top_pos > 0 && v.y >= 0):
+		v *= 0.9
+	
+	
+	if bottom_pos < -20:	# This impulse force should get calculated by the bottom/top_pos and should already get applied on less/more then 0
+		v.y = lerp(v.y, -bottom_pos/10, damping)
+	elif top_pos > 20:
+		v.y = lerp(v.y, -top_pos/10, damping)
+	
 	get_children()[0].rect_position += v
 
 
 func _gui_input(event):
-
-	if event is InputEventMouseButton:
-		match event.button_index:
-			BUTTON_MIDDLE:  is_grabbed = event.pressed
-
-	if event is InputEventMouseButton:
-		match event.button_index:
-			BUTTON_WHEEL_DOWN:  v.y += multi
-			BUTTON_WHEEL_UP:    v.y -= multi
-			BUTTON_WHEEL_RIGHT: v.x += multi
-			BUTTON_WHEEL_LEFT:  v.x -= multi
-
+	#if bottom_pos < -1:
+		if event is InputEventMouseButton:
+			match event.button_index:
+				BUTTON_WHEEL_UP:  v.y -= multi
+				BUTTON_WHEEL_RIGHT: v.x += multi
+				BUTTON_WHEEL_LEFT:  v.x -= multi
+	#elif top_pos > 1:
+		if event is InputEventMouseButton:
+			match event.button_index:
+				BUTTON_WHEEL_DOWN:    v.y += multi
+				BUTTON_WHEEL_RIGHT: v.x += multi
+				BUTTON_WHEEL_LEFT:  v.x -= multi
+	#else:
+		if event is InputEventMouseButton:
+			match event.button_index:
+				BUTTON_WHEEL_DOWN:    v.y += multi
+				BUTTON_WHEEL_UP:  v.y -= multi
+				BUTTON_WHEEL_RIGHT: v.x += multi
+				BUTTON_WHEEL_LEFT:  v.x -= multi
